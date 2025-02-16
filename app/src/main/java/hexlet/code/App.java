500ee45543cfeead57d83fdcd210d7da05254029
Кирилл <kirillvolk@MacBook-Air-Kirill.local>
2025-02-16 18:54:29 +0300

    4 step

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,10 +5,26 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
+import java.nio.file.Path;
+
 @Command(name = "gendiff", mixinStandardHelpOptions = true,
         description = "Compares two configuration files and shows a difference.")
 
-public class App {
+public class App implements Runnable{
+    @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
+    private Path filepath1;
+
+    @Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
+    private Path filepath2;
+
+    @Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format [default: stylish]")
+    String format = "stylish";
+
+    @Override
+    public void run() {
+
+    }
+
     public static void main(String... args) {
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
