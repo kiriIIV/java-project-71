74ed9fe6548cf360f7ddf553981c55175efed2c7
Кирилл <kirillvolk@MacBook-Air-Kirill.local>
2025-02-17 23:45:54 +0300

    fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation(platform("org.junit:junit-bom:5.10.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation ("info.picocli:picocli:4.7.6")
     implementation ("com.fasterxml.jackson.core:jackson-databind:2.15.3")
